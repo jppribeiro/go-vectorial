5dffb001e28ded9b5f712f056cda9316ed1f51a9
João Ribeiro <joaopribeiro@deloitte.pt>
2021-04-25 17:03:13 +0100

    Linting

--- vec3/vec3.go
+++ vec3/vec3.go
@@ -4,106 +4,123 @@ import (
 	"math"
 )
 
-type vec3 struct {
+// Vec3 defines a 3-dimension vector
+type Vec3 struct {
 	I float64
 	J float64
 	K float64
 }
 
-func New(i float64, j float64, k float64) *vec3 {
-	return &vec3{i, j, k}
-}
-
-func (v1 *vec3) Add(v2 vec3) {
+// Add takes a vec3 transforming v1 by adding their dimensions
+func (v1 *Vec3) Add(v2 Vec3) {
 	v1.I += v2.I
 	v1.J += v2.J
 	v1.K += v2.K
 }
 
-func Add(v1 vec3, v2 vec3) *vec3 {
-	return &vec3{v1.I + v2.I, v1.J + v2.J, v1.K + v2.K}
+// Add takes two vec3 returning a new vec3 whose dimensions are equal to the sum of the given vec3 dimensions
+func Add(v1 Vec3, v2 Vec3) *Vec3 {
+	return &Vec3{v1.I + v2.I, v1.J + v2.J, v1.K + v2.K}
 }
 
-func (v1 *vec3) Dot(v2 vec3) float64 {
+// Dot takes a vec3 and returns the dot product of the two vectors
+func (v1 *Vec3) Dot(v2 Vec3) float64 {
 	return Dot(*v1, v2)
 }
 
-func Dot(v1 vec3, v2 vec3) float64 {
+// Dot takes two vec3 and returns the dot product of the two vec3
+func Dot(v1 Vec3, v2 Vec3) float64 {
 	return v1.I*v2.I + v1.J*v2.J + v1.K*v2.K
 }
 
-func (v *vec3) Magnitude() float64 {
-	return Magnitude(*v)
+// Magnitude returns the magnitude of a vec3
+func (v1 *Vec3) Magnitude() float64 {
+	return Magnitude(*v1)
 }
 
-func Magnitude(v vec3) float64 {
+// Magnitude takes a vec3 and returns its magnitude
+func Magnitude(v Vec3) float64 {
 	return math.Sqrt(Dot(v, v))
 }
 
-func (v *vec3) MirrorI() {
-	v.I = -v.I
+// MirrorI transforms v by mirroring its I-Dimension
+func (v1 *Vec3) MirrorI() {
+	v1.I = -v1.I
 }
 
-func MirrorI(v vec3) *vec3 {
-	return &vec3{-v.I, v.J, v.K}
+// MirrorI takes a Vec3 and mirrors its I-dimension returning a new Vec3
+func MirrorI(v Vec3) *Vec3 {
+	return &Vec3{-v.I, v.J, v.K}
 }
 
-func (v *vec3) MirrorJ() {
-	v.J = -v.J
+// MirrorJ transforms v by mirroring its J-Dimension
+func (v1 *Vec3) MirrorJ() {
+	v1.J = -v1.J
 }
 
-func MirrorJ(v vec3) *vec3 {
-	return &vec3{v.I, -v.J, v.K}
+// MirrorJ takes a Vec3 and mirrors its J-dimension returning a new Vec3
+func MirrorJ(v Vec3) *Vec3 {
+	return &Vec3{v.I, -v.J, v.K}
 }
 
-func (v *vec3) MirrorK() {
-	v.K = -v.K
+// MirrorK transforms v by mirroring its K-Dimension
+func (v1 *Vec3) MirrorK() {
+	v1.K = -v1.K
 }
 
-func MirrorK(v vec3) *vec3 {
-	return &vec3{v.I, v.J, -v.K}
+// MirrorK takes a Vec3 and mirrors its K-dimension returning a new Vec3
+func MirrorK(v Vec3) *Vec3 {
+	return &Vec3{v.I, v.J, -v.K}
 }
 
-func (v *vec3) Mirror() {
-	v.MirrorI()
-	v.MirrorJ()
-	v.MirrorK()
+// Mirror transforms v by mirroring all of its dimensions
+func (v1 *Vec3) Mirror() {
+	v1.MirrorI()
+	v1.MirrorJ()
+	v1.MirrorK()
 }
 
-func Mirror(v vec3) *vec3 {
-	return &vec3{-v.I, -v.J, -v.K}
+// Mirror takes a Vec3 and mirrors all of its dimensions, returning a new Vec3
+func Mirror(v Vec3) *Vec3 {
+	return &Vec3{-v.I, -v.J, -v.K}
 }
 
-func (v *vec3) Scale(s float64) {
-	v.I *= s
-	v.J *= s
-	v.K *= s
+// Scale scales v by an s amount
+func (v1 *Vec3) Scale(s float64) {
+	v1.I *= s
+	v1.J *= s
+	v1.K *= s
 }
 
-func Scale(v vec3, s float64) *vec3 {
-	return &vec3{v.I * s, v.J * s, v.K * s}
+// Scale scales a given Vec3 by an s amount, returning a new Vec3
+func Scale(v Vec3, s float64) *Vec3 {
+	return &Vec3{v.I * s, v.J * s, v.K * s}
 }
 
-func (v1 *vec3) Sum(v2 vec3) {
+// Sum takes a Vec3 and adds it to the underlying Vec3 transforming it
+func (v1 *Vec3) Sum(v2 Vec3) {
 	v1.I += v2.I
 	v1.J += v2.J
 	v1.K += v2.K
 }
 
-func Sum(v1 vec3, v2 vec3) *vec3 {
-	return &vec3{v1.I + v2.I, v1.J + v2.J, v1.K + v2.K}
+// Sum takes two Vec3 and returns their sum as a new Vec3
+func Sum(v1 Vec3, v2 Vec3) *Vec3 {
+	return &Vec3{v1.I + v2.I, v1.J + v2.J, v1.K + v2.K}
 }
 
-func (v *vec3) Unit() {
-	m := v.Magnitude()
+// Unit tranforms a Vec3 into a unit vector
+func (v1 *Vec3) Unit() {
+	m := Magnitude(*v1)
 
-	v.I /= m
-	v.J /= m
-	v.K /= m
+	v1.I /= m
+	v1.J /= m
+	v1.K /= m
 }
 
-func Unit(v vec3) *vec3 {
+// Unit takes a Vec3 and returns a unit vector
+func Unit(v Vec3) *Vec3 {
 	m := Magnitude(v)
 
-	return &vec3{v.I / m, v.J / m, v.K / m}
+	return &Vec3{v.I / m, v.J / m, v.K / m}
 }
--- vec3/vec3_test.go
+++ vec3/vec3_test.go
@@ -6,65 +6,39 @@ import (
 	"testing"
 )
 
-func TestNew(t *testing.T) {
-	type args struct {
-		i float64
-		j float64
-		k float64
-	}
-	tests := []struct {
-		name string
-		args args
-		want *vec3
-	}{
-		{
-			name: "return vec3 with expected fields",
-			args: args{1, 2, 3},
-			want: &vec3{1, 2, 3},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := New(tt.args.i, tt.args.j, tt.args.k); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("New() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_vec3_Add(t *testing.T) {
+func Test_Vec3_Add(t *testing.T) {
 	type fields struct {
 		I float64
 		J float64
 		K float64
 	}
 	type args struct {
-		v2 vec3
+		v2 Vec3
 	}
 
 	v := fields{1, 1, 1}
 
-	args1 := args{vec3{1, 2, 3}}
-	args2 := args{vec3{-1, 2, 3}}
-	args3 := args{vec3{-2, -3, 3}}
-	args4 := args{vec3{0, 0, 0}}
-	args5 := args{vec3{-1, -1, -1}}
+	args1 := args{Vec3{1, 2, 3}}
+	args2 := args{Vec3{-1, 2, 3}}
+	args3 := args{Vec3{-2, -3, 3}}
+	args4 := args{Vec3{0, 0, 0}}
+	args5 := args{Vec3{-1, -1, -1}}
 
 	tests := []struct {
 		name   string
 		fields fields
 		args   args
-		want   vec3
+		want   Vec3
 	}{
-		{"v2: {1, 2, 3}", v, args1, vec3{2, 3, 4}},
-		{"v2: {-1, 2, 3}", v, args2, vec3{0, 3, 4}},
-		{"v2: {-2, -3, 3}", v, args3, vec3{-1, -2, 4}},
-		{"v2: {0, 0, 0}", v, args4, vec3{1, 1, 1}},
-		{"v2: {-2, -3, 3}", v, args5, vec3{0, 0, 0}},
+		{"v2: {1, 2, 3}", v, args1, Vec3{2, 3, 4}},
+		{"v2: {-1, 2, 3}", v, args2, Vec3{0, 3, 4}},
+		{"v2: {-2, -3, 3}", v, args3, Vec3{-1, -2, 4}},
+		{"v2: {0, 0, 0}", v, args4, Vec3{1, 1, 1}},
+		{"v2: {-2, -3, 3}", v, args5, Vec3{0, 0, 0}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			v1 := &vec3{
+			v1 := &Vec3{
 				I: tt.fields.I,
 				J: tt.fields.J,
 				K: tt.fields.K,
@@ -78,14 +52,14 @@ func Test_vec3_Add(t *testing.T) {
 	}
 }
 
-func Test_vec3_Dot(t *testing.T) {
+func Test_Vec3_Dot(t *testing.T) {
 	type fields struct {
 		I float64
 		J float64
 		K float64
 	}
 	type args struct {
-		v2 vec3
+		v2 Vec3
 	}
 	tests := []struct {
 		name   string
@@ -93,25 +67,25 @@ func Test_vec3_Dot(t *testing.T) {
 		args   args
 		want   float64
 	}{
-		{"for v1:{1, 1, 1} and v2:{1, 1, 1} expect 3", fields{1, 1, 1}, args{vec3{1, 1, 1}}, 3},
-		{"for v1:{2, 3, 4} and v2:{-1, -1, 1} expect -1", fields{2, 3, 4}, args{vec3{-1, -1, 1}}, -1},
-		{"for v1:{Pi, 1, 1} and v2:{1, 2, 0} expect 2Pi", fields{math.Pi, 1, 1}, args{vec3{1, 2, 0}}, math.Pi + 2},
+		{"for v1:{1, 1, 1} and v2:{1, 1, 1} expect 3", fields{1, 1, 1}, args{Vec3{1, 1, 1}}, 3},
+		{"for v1:{2, 3, 4} and v2:{-1, -1, 1} expect -1", fields{2, 3, 4}, args{Vec3{-1, -1, 1}}, -1},
+		{"for v1:{Pi, 1, 1} and v2:{1, 2, 0} expect 2Pi", fields{math.Pi, 1, 1}, args{Vec3{1, 2, 0}}, math.Pi + 2},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			v1 := &vec3{
+			v1 := &Vec3{
 				I: tt.fields.I,
 				J: tt.fields.J,
 				K: tt.fields.K,
 			}
 			if got := v1.Dot(tt.args.v2); got != tt.want {
-				t.Errorf("vec3.Dot() = %v, want %v", got, tt.want)
+				t.Errorf("Vec3.Dot() = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
 
-func Test_vec3_Magnitude(t *testing.T) {
+func Test_Vec3_Magnitude(t *testing.T) {
 	type fields struct {
 		I float64
 		J float64
@@ -129,19 +103,19 @@ func Test_vec3_Magnitude(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			v := &vec3{
+			v := &Vec3{
 				I: tt.fields.I,
 				J: tt.fields.J,
 				K: tt.fields.K,
 			}
 			if got := v.Magnitude(); got != tt.want {
-				t.Errorf("vec3.Magnitude() = %v, want %v", got, tt.want)
+				t.Errorf("Vec3.Magnitude() = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
 
-func Test_vec3_Mirror(t *testing.T) {
+func Test_Vec3_Mirror(t *testing.T) {
 	type fields struct {
 		I float64
 		J float64
@@ -150,14 +124,14 @@ func Test_vec3_Mirror(t *testing.T) {
 	tests := []struct {
 		name   string
 		fields fields
-		want   vec3
+		want   Vec3
 	}{
-		{"v{1, 1, 1} -> v{-1, -1, -1}", fields{1, 1, 1}, vec3{-1, -1, -1}},
-		{"v{0, 0, 0} -> v{0, 0, 0}", fields{0, 0, 0}, vec3{0, 0, 0}},
+		{"v{1, 1, 1} -> v{-1, -1, -1}", fields{1, 1, 1}, Vec3{-1, -1, -1}},
+		{"v{0, 0, 0} -> v{0, 0, 0}", fields{0, 0, 0}, Vec3{0, 0, 0}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			v := &vec3{
+			v := &Vec3{
 				I: tt.fields.I,
 				J: tt.fields.J,
 				K: tt.fields.K,
@@ -165,13 +139,13 @@ func Test_vec3_Mirror(t *testing.T) {
 			v.Mirror()
 
 			if !reflect.DeepEqual(*v, tt.want) {
-				t.Errorf("vec3.Mirror() = %v, want %v", *v, tt.want)
+				t.Errorf("Vec3.Mirror() = %v, want %v", *v, tt.want)
 			}
 		})
 	}
 }
 
-func Test_vec3_Scale(t *testing.T) {
+func Test_Vec3_Scale(t *testing.T) {
 	type fields struct {
 		I float64
 		J float64
@@ -184,16 +158,16 @@ func Test_vec3_Scale(t *testing.T) {
 		name   string
 		fields fields
 		args   args
-		want   vec3
+		want   Vec3
 	}{
-		{"v{1, 1, 1}; s = 2 -> v{2, 2, 2}", fields{1, 1, 1}, args{2}, vec3{2, 2, 2}},
-		{"v{0, 0, 0}; s = 2 -> v{0, 0, 0}", fields{0, 0, 0}, args{2}, vec3{0, 0, 0}},
-		{"v{1, 1, 1}; s = 0 -> v{0, 0, 0}", fields{1, 1, 1}, args{0}, vec3{0, 0, 0}},
-		{"v{0.501, 1, 1}; s = 2 -> v{1.002, 2, 2}", fields{0.501, 1, 1}, args{2}, vec3{1.002, 2, 2}},
+		{"v{1, 1, 1}; s = 2 -> v{2, 2, 2}", fields{1, 1, 1}, args{2}, Vec3{2, 2, 2}},
+		{"v{0, 0, 0}; s = 2 -> v{0, 0, 0}", fields{0, 0, 0}, args{2}, Vec3{0, 0, 0}},
+		{"v{1, 1, 1}; s = 0 -> v{0, 0, 0}", fields{1, 1, 1}, args{0}, Vec3{0, 0, 0}},
+		{"v{0.501, 1, 1}; s = 2 -> v{1.002, 2, 2}", fields{0.501, 1, 1}, args{2}, Vec3{1.002, 2, 2}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			v := &vec3{
+			v := &Vec3{
 				I: tt.fields.I,
 				J: tt.fields.J,
 				K: tt.fields.K,
@@ -201,35 +175,35 @@ func Test_vec3_Scale(t *testing.T) {
 			v.Scale(tt.args.s)
 
 			if !reflect.DeepEqual(*v, tt.want) {
-				t.Errorf("vec3.Scale() = %v, want %v", *v, tt.want)
+				t.Errorf("Vec3.Scale() = %v, want %v", *v, tt.want)
 			}
 		})
 	}
 }
 
-func Test_vec3_Sum(t *testing.T) {
+func Test_Vec3_Sum(t *testing.T) {
 	type fields struct {
 		I float64
 		J float64
 		K float64
 	}
 	type args struct {
-		v2 vec3
+		v2 Vec3
 	}
 	tests := []struct {
 		name   string
 		fields fields
 		args   args
-		want   vec3
+		want   Vec3
 	}{
-		{"v1{1, 1, 1} + v2{1, 1, 1} -> v1{2, 2, 2}", fields{1, 1, 1}, args{vec3{1, 1, 1}}, vec3{2, 2, 2}},
-		{"v1{1, 1, 1} + v2{-1, -1, -1} -> v1{0, 0, 0}", fields{1, 1, 1}, args{vec3{-1, -1, -1}}, vec3{0, 0, 0}},
-		{"v1{0, 0, 0} + v2{-1, -1, -1} -> v1{-1, -1, -1}", fields{0, 0, 0}, args{vec3{-1, -1, -1}}, vec3{-1, -1, -1}},
-		{"v1{1, 1, 1} + v2{0, 0, 0} -> v1{1, 1, 1}", fields{1, 1, 1}, args{vec3{0, 0, 0}}, vec3{1, 1, 1}},
+		{"v1{1, 1, 1} + v2{1, 1, 1} -> v1{2, 2, 2}", fields{1, 1, 1}, args{Vec3{1, 1, 1}}, Vec3{2, 2, 2}},
+		{"v1{1, 1, 1} + v2{-1, -1, -1} -> v1{0, 0, 0}", fields{1, 1, 1}, args{Vec3{-1, -1, -1}}, Vec3{0, 0, 0}},
+		{"v1{0, 0, 0} + v2{-1, -1, -1} -> v1{-1, -1, -1}", fields{0, 0, 0}, args{Vec3{-1, -1, -1}}, Vec3{-1, -1, -1}},
+		{"v1{1, 1, 1} + v2{0, 0, 0} -> v1{1, 1, 1}", fields{1, 1, 1}, args{Vec3{0, 0, 0}}, Vec3{1, 1, 1}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			v1 := &vec3{
+			v1 := &Vec3{
 				I: tt.fields.I,
 				J: tt.fields.J,
 				K: tt.fields.K,
@@ -237,13 +211,13 @@ func Test_vec3_Sum(t *testing.T) {
 			v1.Sum(tt.args.v2)
 
 			if !reflect.DeepEqual(*v1, tt.want) {
-				t.Errorf("vec3.Scale() = %v, want %v", *v1, tt.want)
+				t.Errorf("Vec3.Scale() = %v, want %v", *v1, tt.want)
 			}
 		})
 	}
 }
 
-func Test_vec3_Unit(t *testing.T) {
+func Test_Vec3_Unit(t *testing.T) {
 	type fields struct {
 		I float64
 		J float64
@@ -252,14 +226,14 @@ func Test_vec3_Unit(t *testing.T) {
 	tests := []struct {
 		name   string
 		fields fields
-		want   vec3
+		want   Vec3
 	}{
-		{"v{2,0,0} -> v{1,0,0}", fields{2, 0, 0}, vec3{1, 0, 0}},
-		{"v{1,1,1} -> v{1/sqr(3),1/sqr(3),1/sqr(3)}", fields{1, 1, 1}, vec3{1 / math.Sqrt(3), 1 / math.Sqrt(3), 1 / math.Sqrt(3)}},
+		{"v{2,0,0} -> v{1,0,0}", fields{2, 0, 0}, Vec3{1, 0, 0}},
+		{"v{1,1,1} -> v{1/sqr(3),1/sqr(3),1/sqr(3)}", fields{1, 1, 1}, Vec3{1 / math.Sqrt(3), 1 / math.Sqrt(3), 1 / math.Sqrt(3)}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			v := &vec3{
+			v := &Vec3{
 				I: tt.fields.I,
 				J: tt.fields.J,
 				K: tt.fields.K,
@@ -267,7 +241,7 @@ func Test_vec3_Unit(t *testing.T) {
 			v.Unit()
 
 			if !reflect.DeepEqual(*v, tt.want) {
-				t.Errorf("vec3.Unit() = %v, want %v", *v, tt.want)
+				t.Errorf("Vec3.Unit() = %v, want %v", *v, tt.want)
 			}
 		})
 	}
